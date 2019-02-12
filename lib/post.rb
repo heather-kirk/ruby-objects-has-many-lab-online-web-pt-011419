ca4ec40ff89f9d59aaca6f4c0327552712141918
Heather Kirk <hskirk@gmail.com>
2019-02-12 01:15:48 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,12 @@
 class Post
   
-  attr_accessor :title
+  attr_accessor :title, :author 
   
   @@all = []
   
   def initialize(title) 
     @title = title
+    @author = author 
     @@all << self 
   end 
   
