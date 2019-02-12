522cdf4617f0c745e0d4c19683ea68424dea0878
Heather Kirk <hskirk@gmail.com>
2019-02-12 01:05:00 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -4,4 +4,17 @@ class Author
   def initialize(name)
     @name = name 
   end 
+  
+  def posts 
+  end 
+  
+  def add_post 
+  end 
+  
+  def add_post_by_title 
+  end 
+  
+  def self.post_count
+    Post.count 
+  end 
 end 
